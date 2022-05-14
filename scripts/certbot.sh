63e6275460168f0e7e94e44a9629b1d638fe4b29
isaiah-v <isaiah.v@comcast.net>
2022-05-14 06:38:01 +0000

    Added snipe-it

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -2,7 +2,7 @@
 
 #examples
 # ./certbot.sh renew --dry-run
-# ./certbot.sh --nginx --expand --non-interactive --agree-tos -m isaiah.v@comcast.net -d ivcode.org -d auth.ivcode.org -d wiki.ivcode.org -d nextcloud.ivcode.org
+# ./certbot.sh --nginx --expand --non-interactive --agree-tos -m isaiah.v@comcast.net -d ivcode.org -d auth.ivcode.org -d wiki.ivcode.org -d nextcloud.ivcode.org -d snipeit.ivcode.org
 
 ARG="certbot $@"
 docker exec -it nginx $ARG
