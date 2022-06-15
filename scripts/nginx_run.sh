f1f6c57daa471137ca38b59b6965e2e411824d53
isaiah-v <isaiah.v@comcast.net>
2022-06-14 20:17:18 -0700

    Set container to restart unless stopped

--- scripts/nginx_run.sh
+++ scripts/nginx_run.sh
@@ -4,6 +4,7 @@ docker run \
   -d \
   --name organize-me-nginx \
   --network organize_me_network \
+  --restart unless-stopped \
   -v $ORGANIZE_ME_HOME/nginx/nginx.conf:/etc/nginx/nginx.conf \
   -p 80:80 \
   -p 443:443 \
