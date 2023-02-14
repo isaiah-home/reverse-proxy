41f1cd1778e7e59eaff2cbb7496fbfbfcff56c4b
isaiah-v <isaiah.v@comcast.net>
2023-02-13 20:29:57 -0800

    updated run script

--- scripts/nginx_run.sh
+++ scripts/nginx_run.sh
@@ -4,6 +4,7 @@ docker run \
   -d \
   --name organize-me-nginx \
   --network organize_me_network \
+  --ip 172.22.0.10 \
   --restart unless-stopped \
   -v $ORGANIZE_ME_HOME/nginx/nginx.conf:/etc/nginx/nginx.conf \
   -p 80:80 \
