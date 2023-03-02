89bddbe45556845f0b14235ee9cf1b889a81de23
isaiah-v <isaiah.v@comcast.net>
2023-03-02 01:01:19 -0800

    added vaultwarden

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -2,7 +2,7 @@
 
 #examples
 # ./certbot.sh renew --dry-run
-# ./certbot.sh --nginx --expand --non-interactive --agree-tos -m isaiah.v@comcast.net -d vanderelst.house -d auth.vanderelst.house -d wiki.vanderelst.house -d nextcloud.vanderelst.house -d snipeit.vanderelst.house
+# ./certbot.sh --nginx --expand --non-interactive --agree-tos -m isaiah.v@comcast.net -d vanderelst.house -d auth.vanderelst.house -d wiki.vanderelst.house -d nextcloud.vanderelst.house -d snipeit.vanderelst.house -d vaultwarden.vanderelst.house
 
 ARG="certbot $@"
 docker exec -it organize-me-nginx $ARG
