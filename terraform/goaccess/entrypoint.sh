55429849eb8cff054a426851a6810128cdd82710
isaiah.v <isaiah.v@comcast.net>
2024-10-12 21:08:43 -0700

    test

--- terraform/goaccess/entrypoint.sh
+++ terraform/goaccess/entrypoint.sh
@@ -14,7 +14,7 @@ if [ -n "$MAXMIND_LICENSE_KEY" ]; then
     --output=/usr/local/goaccess/html/index.html \
     --geoip-database=$GEO_IP_DB_PATH/GeoLite2-City.mmdb \
     --persist \
-    --db-path=/usr/local/goaccess/db/goaccess.db \
+    --db-path=/usr/local/goaccess/db/ \
     --restore"
 else
   ARGS="\
@@ -23,7 +23,7 @@ else
       --real-time-html \
       --output=/usr/local/goaccess/html/index.html \
       --persist \
-      --db-path=/usr/local/goaccess/db/goaccess.db \
+      --db-path=/usr/local/goaccess/db/ \
       --restore"
 fi
 
