6a556fb8eb58086e912ddfbf14eb77ea5b65bb61
isaiah.v <isaiah.v@comcast.net>
2024-10-12 20:29:43 -0700

    fixed arg assignments

--- terraform/goaccess/entrypoint.sh
+++ terraform/goaccess/entrypoint.sh
@@ -7,20 +7,22 @@ if [ -n "$MAXMIND_LICENSE_KEY" ]; then
     exit 1
   fi
 
-  ARGS="--log-file=/usr/local/goaccess/logs/access.log" \
-    "--log-format=COMBINED" \
-    "--real-time-html" \
-    "--output=/usr/local/goaccess/html/index.html" \
-    "--geoip-database=$GEO_IP_DB_PATH/GeoLite2-City.mmdb" \
-    "--persist" \
-    "--restore"
+  ARGS="\
+    --log-file=/usr/local/goaccess/logs/access.log \
+    --log-format=COMBINED \
+    --real-time-html \
+    --output=/usr/local/goaccess/html/index.html \
+    --geoip-database=$GEO_IP_DB_PATH/GeoLite2-City.mmdb \
+    --persist \
+    --restore"
 else
-  ARGS="--log-file=/usr/local/goaccess/logs/access.log" \
-      "--log-format=COMBINED" \
-      "--real-time-html" \
-      "--output=/usr/local/goaccess/html/index.html" \
-      "--persist" \
-      "--restore"
+  ARGS="\
+      --log-file=/usr/local/goaccess/logs/access.log \
+      --log-format=COMBINED \
+      --real-time-html \
+      --output=/usr/local/goaccess/html/index.html \
+      --persist \
+      --restore"
 fi
 
 # shellcheck disable=SC2086
